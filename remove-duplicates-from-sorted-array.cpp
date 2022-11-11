9f58010b9191921b076699bb7758699fb1940200
Mahade Hasan <mahadehasan31@gmail.com>
2022-11-11 23:35:25 +0600

    Update remove-duplicates-from-sorted-array.cpp

--- remove-duplicates-from-sorted-array.cpp
+++ remove-duplicates-from-sorted-array.cpp
@@ -4,7 +4,10 @@ Problem: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 Editorial:
 Trick to solve this problem is there can be at most 201 unique numbers in the array.
 
-So for first 201 numbers in the array, we checked which ones are the duplicates, meaning there is already the same number before this one. If a number is a duplicate then we replaced that with 200, 200 is a flag here to remember that this place can be replaced with another number. Then for each elements of the array we check if this is the first occurence, if it is then we put it to the first available place (in place of first 200 we can find).
+So for first 201 numbers in the array, we checked which ones are the duplicates, meaning there is already the same number before this one. 
+If a number is a duplicate then we replaced that with 200, 200 is a flag here to remember that this place can be replaced with another number. 
+Then for each elements of the array we check if this is the first occurence, 
+if it is then we put it to the first available place (in place of first 200 we can find).
 
 So the overall complexity is O(201 * n).
 And space complexity in O(1) because we did not use any auxilary space as asked. 
