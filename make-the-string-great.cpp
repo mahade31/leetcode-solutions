1ec72203d591b6b0f3da902e4565b2088d1d9752
Mahade Hasan <mahadehasan31@gmail.com>
2022-11-11 23:32:50 +0600

    Update make-the-string-great.cpp

--- make-the-string-great.cpp
+++ make-the-string-great.cpp
@@ -3,7 +3,9 @@ Problem: https://leetcode.com/problems/make-the-string-great/
 
 Editorial:
 We can use stack to solve the problem. 
-We iterate through every character of the string, and for each character we see if the top character of the stack is matched with the current character. If matched, we pop the top character and go to the next character. Here two character is said to be mathced if one of them is uppercase and the other one is lowercase also they are the same alphabet. 
+We iterate through every character of the string, and for each character we see if the top character of the stack is matched 
+with the current character. If matched, we pop the top character and go to the next character. Here two character is said to be 
+mathced if one of them is uppercase and the other one is lowercase also they are the same alphabet. 
 
 */
 
