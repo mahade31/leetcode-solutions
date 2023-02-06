4217f12bd7dd3485c9ed57d187324029b21c1752
Mahade Hasan <mahadehasan31@gmail.com>
2023-02-07 01:06:34 +0600

    added

---
+++ shuffle-the-array.cpp
@@ -0,0 +1,13 @@
+//solution 1:
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector <int> ret;
+        for (int i = 0; i < n; ++i){
+            ret.push_back(nums[i]);
+            ret.push_back(nums[i + n]);
+        }
+        return ret;
+    }
+};
