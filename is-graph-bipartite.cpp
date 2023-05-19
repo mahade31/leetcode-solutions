793bbfc5381147cf08743bbd9961a930c84bf9bc
Mahade Hasan <mahadehasan31@gmail.com>
2023-05-19 16:07:43 +0600

    editorial added

--- is-graph-bipartite.cpp
+++ is-graph-bipartite.cpp
@@ -1,5 +1,13 @@
 //problem: https://leetcode.com/problems/is-graph-bipartite
+/*
 
+This problem can be solved if we find that if the nodes of the graph can be colored using two colors such that not adjacent nodes has the same color.
+
+If we can color a graph using this constraints, then we divide the nodes in two sets. One color in one set and other color in another set. 
+
+So we start with a node by coloring it with any color and try to color the neighboring nodes with different color. We keep trying to do these for every node. If we fail then the graph is not bipartite. To implement this we can modify BFS a bit.
+
+*/
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
