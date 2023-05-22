e74b30662162269792eed35e492453be515288b0
Mahade Hasan <mahadehasan31@gmail.com>
2023-05-22 23:12:36 +0600

    solution 2 added

--- top-k-frequent-elements.cpp
+++ top-k-frequent-elements.cpp
@@ -1,5 +1,7 @@
 //problem: https://leetcode.com/problems/top-k-frequent-elements
 
+
+//solution 1: O(n log n) where n = 20000(range of given numbers)
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -13,6 +15,27 @@ public:
             vec.push_back({-cnt[i], i - 10000});
         }
         sort (vec.begin(), vec.end());
+        vector <int> ret;
+        for (int i = 0; i < k; ++i)
+            ret.push_back(vec[i].second);
+        return ret;
+    }
+};
+
+//solution 2: O(n log n) where n = length of the list.
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map <int, int> cnt;
+        for (int a : nums) {
+            ++cnt[a];
+        }
+        vector <pair<int, int>> vec;
+        for (auto it : cnt){
+            vec.push_back({it.second, it.first});
+        }
+        sort (vec.rbegin(), vec.rend());
         vector <int> ret;
         for (int i = 0; i < k; ++i)
             ret.push_back(vec[i].second);
