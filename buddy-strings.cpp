77ec61688a50b1c42093fe69861fe044add7da24
Mahade Hasan <mahadehasan31@gmail.com>
2023-07-03 22:50:50 +0600

    second solution added

--- buddy-strings.cpp
+++ buddy-strings.cpp
@@ -1,5 +1,6 @@
 //problem: https://leetcode.com/problems/buddy-strings
 
+//O(n*log(n))
 class Solution {
 public:
     bool buddyStrings(string s, string goal) {
@@ -24,6 +25,40 @@ public:
         }
         else if (cnt == 0) {
             return st.size() != s.size();
+        }
+        return false;
+    }
+};
+
+//O(n)
+
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if (s.size() != goal.size()) return false;
+
+        int len = s.size();
+        int a = -1, b = -1, cnt = 0;
+        int st[26];
+        memset(st, 0, sizeof st);
+
+        for (int i = 0; i < len; ++i) {
+            ++st[s[i] - 'a'];
+            if (s[i] != goal[i]) {
+                if (a == -1) a = i;
+                else b = i;
+                ++cnt;
+            }
+        }
+
+        if (cnt == 2) {
+            swap(s[a], s[b]);
+            return s == goal;
+        }
+        else if (cnt == 0) {
+            for (int i = 0; i < 26; ++i) {
+                if (st[i] >= 2) return true;
+            }
         }
         return false;
     }
