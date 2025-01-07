7eee33dac38f852ea8add1a1c99e11168d6280e3
mahade31 <mahadehasan31@gmail.com>
2025-01-07 15:42:27 +0600

    solution added

---
+++ string-matching-in-an-array.cs
@@ -0,0 +1,20 @@
+/*
+Problem: https://leetcode.com/problems/string-matching-in-an-array/description/
+
+
+Solution:
+*/
+public class Solution {
+    public IList<string> StringMatching(string[] words) {
+        List <string> ret = new List<string>();
+        for (int i = 0; i < words.Count(); ++i) {
+            for (int j = 0; j < words.Count(); ++j) {
+                if (i != j && words[j].Contains(words[i])) {
+                    ret.Add(words[i]);
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+}
