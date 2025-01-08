b00d535158094c0bcd26ec281f83df0e1b5baa4f
mahade31 <mahadehasan31@gmail.com>
2025-01-08 10:49:33 +0600

    Solution added

---
+++ count-prefix-and-suffix-pairs-i.cs
@@ -0,0 +1,18 @@
+/*
+Problem: https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/
+
+Solution:
+*/
+public class Solution {
+    public int CountPrefixSuffixPairs(string[] words) {
+        int ret = 0;
+        for (int i = 0; i < words.Count(); ++i) {
+            for (int j = i + 1; j < words.Count(); ++j) {
+                if (words[j].StartsWith(words[i]) && words[j].EndsWith(words[i])) {
+                    ++ret;
+                }
+            }
+        }
+        return ret;
+    }
+}
