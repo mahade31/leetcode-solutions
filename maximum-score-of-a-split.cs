6bd863ba618d9759c71c11926352ca611272dbd7
Mahade Hasan <mahadehasan31@gmail.com>
2026-01-03 01:10:26 +0600

    add

---
+++ maximum-score-of-a-split.cs
@@ -0,0 +1,24 @@
+//problem: https://leetcode.com/problems/maximum-score-of-a-split
+
+//solution:
+public class Solution {
+    public long MaximumScore(int[] nums) {
+        long sum = 0;
+        foreach (var num in nums) {
+            sum += num;
+        }
+
+        long min = (long)1e10;
+        long ret = (long)-1e15;
+        int n = nums.Count();
+
+        for (int i = n - 2; i >= 0; --i) {
+            min = Math.Min(min, nums[i + 1]);
+            sum -= nums[i + 1];
+
+            ret = Math.Max(ret, sum - min);
+        }
+
+        return ret;
+    }
+}
